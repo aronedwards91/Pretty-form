a46ba1bb37a096eba25be54dd2184dcf82ea1e9a
aron.edwards <aronedwards91@protonmail.com>
2025-08-12 20:48:56 +0100

    clean up form

--- src/cyoa-app.jsx
+++
@@ -1,8 +0,0 @@
-export default function CyoaApp() {
-    return (
-        <div className="bg-blue-200 rounded-lg shadow-xl p-2 max-w-2xl w-full flex justify-center">
-            <h1>Cyoa App</h1>
-            <iframe id="cross_domain_page" src="http://localhost:3000/form/" height="500px" width="500px"></iframe>
-        </div>
-    );
-}
