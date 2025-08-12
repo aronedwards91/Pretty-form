aa4b129cb24614b9f0b270e6c94d8ce81564174f
aron.edwards <aronedwards91@protonmail.com>
2025-08-12 20:49:55 +0100

    pass form data to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
 import schema from "../form/example-data-schema.json";
 import { Form } from "@rjsf/daisyui";
 import validator from "@rjsf/validator-ajv8";
-import CyoaApp from "./cyoa-app";
+import initFormData from "./init-form-data.json";
 
 function App() {
+  console.log('>>>initFormData', initFormData);
+
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
+    console.log('>>>handleSubmit', e);
+    msgToIframe(e, e.formData);
   };
 
   function msgToIframe(event, data) {
-    console.log(msgToIframe, {
+    console.log("msgToIframe", {
       event,
-      data,
+      data,     
     });
-
+    
+    // include type:config
     document
       .getElementById("cross_domain_page")
-      .contentWindow.postMessage({ test: "value" }, window.location.origin);
+      .contentWindow.postMessage({type:"config", config: data}, "http://localhost:5173");
   }
 
   return (
     <main className="bg-gray-100 min-h-screen w-full flex flex-row">
       <div className="w-1/2 mx-auto">
         <div className="bg-white p-8 max-w-2xl w-full flex justify-center">
-          <Form schema={schema} validator={validator} onSubmit={handleSubmit} />
+          <Form schema={schema} validator={validator} onSubmit={handleSubmit} formData={initFormData} />
         </div>
       </div>
 
       <div className="w-1/2 mx-auto">
-        <button
-          onClick={() => {
-            msgToIframe();
-          }}
-        >
-          Test Post Message
-        </button>
-
-        <CyoaApp />
+        <iframe id="cross_domain_page" src="http://localhost:5173?src=msg" height="500px" width="500px"></iframe>
       </div>
     </main>
   );
