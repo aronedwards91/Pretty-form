f94379c3d05e1c35773fe75c1eb2af8e00b1dff8
aron.edwards <aronedwards91@protonmail.com>
2025-08-13 21:04:53 +0100

    initialise with preloaded form

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,75 @@ import schema from "../form/example-data-schema.json";
 import { Form } from "@rjsf/daisyui";
 import validator from "@rjsf/validator-ajv8";
 import initFormData from "./init-form-data.json";
-
+import { useEffect, useRef } from "react";
 
 function App() {
-  console.log('>>>initFormData', initFormData);
-  console.log('>>>import.meta.env.VITE_APP_URL', import.meta.env.VITE_APP_URL);
+  console.log(">>>initFormData", initFormData);
+  console.log(">>>import.meta.env.VITE_APP_URL", import.meta.env.VITE_APP_URL);
 
   const handleSubmit = (e) => {
-    console.log('>>>handleSubmit', e);
-    msgToIframe(e, e.formData);
+    console.log(">>>handleSubmit", e);
+    msgToIframe(e.formData);
   };
 
-  function msgToIframe(event, data) {
-    console.log("msgToIframe", {
-      event,
-      data,     
-    });
-    
+  function msgToIframe(data) {
     // include type:config
-    document
-      .getElementById("cross_domain_page")
-      .contentWindow.postMessage({type:"config", config: data}, import.meta.env.VITE_APP_URL);
+    const iframe = document.getElementById("cross_domain_page");
+    console.log("1>>>msgToIframe", data);
+    if (iframe) {
+      iframe.contentWindow.postMessage(
+        { type: "config", config: data },
+        import.meta.env.VITE_APP_URL
+      );
+    }
   }
 
+  function initialFormLoad() {
+    msgToIframe(initFormData);
+    setTimeout(() => {
+      msgToIframe(initFormData);
+    }, 2000);
+  }
+
+  const iframeRef = useRef(null);
+
+  // workaround for onload firing before iframe is loaded
+  useEffect(() => {
+    if (iframeRef.current) {
+      iframeRef.current.src = import.meta.env.VITE_APP_URL;
+    }
+    iframeRef.current?.addEventListener("load", initialFormLoad);
+    return () => {
+      iframeRef.current?.removeEventListener("load", initialFormLoad);
+    };
+  }, []);
+
   return (
-    <main className="bg-gray-100 min-h-screen w-full flex flex-row" style={{width: '100vw'}}>
+    <main
+      className="bg-gray-100 min-h-screen w-full flex flex-row"
+      style={{ width: "100vw" }}
+    >
       <div className="w-1/2 mx-auto bg-gray-200 max-h-screen h-screen overflow-y-scroll">
         <div className="p-8 max-w-2xl w-full flex justify-center">
-          <Form schema={schema} validator={validator} onSubmit={handleSubmit} formData={initFormData} />
+          <Form
+            schema={schema}
+            validator={validator}
+            onSubmit={handleSubmit}
+            formData={initFormData}
+          />
         </div>
       </div>
 
       <div className="w-1/2 mx-auto max-h-screen h-screen bg-gray-300">
         <div className="w-full h-full overflow-y-scroll">
-          <iframe id="cross_domain_page" src={`${import.meta.env.VITE_APP_URL}?src=msg`} height="100%" width="100%"></iframe>
+          <iframe
+            ref={iframeRef}
+            id="cross_domain_page"
+            src={`${import.meta.env.VITE_APP_URL}?src=msg`}
+            height="100%"
+            width="100%"
+            onLoad={initialFormLoad}
+          ></iframe>
         </div>
       </div>
     </main>
